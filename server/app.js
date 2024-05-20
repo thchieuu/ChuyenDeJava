4d0b26eca6f06ad6305a1efab0ea243090b06001
Thanh Dat <caodat2772002@gmail.com>
2024-05-20 13:27:11 +0700

    feat: Chức năng đăng nhập và đăng ký

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const authRoutes = require('./src/routes/authRoutes.js');
 
 const app = express();
 
@@ -14,6 +14,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/news', {
 .then(() => console.log('Kết nối database thành công'))
 .catch(err => console.error('Lỗi kết nối database:', err));
 
+// Sử dụng middleware
+app.use(cors());
+app.use(express.json());
+
+// Sử dụng route
+app.use('/api/auth', authRoutes);
 
 // Khởi động server
 const port = process.env.PORT || 7000;
