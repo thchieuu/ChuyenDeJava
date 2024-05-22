9e826c38d09fe8f0a9890abcfac2fcdaa7bfbfc1
Thanh Dat <caodat2772002@gmail.com>
2024-05-22 16:25:48 +0700

    feat: chức năng comment và like/unlike comment

---
+++ server/src/routes/commentRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+const authMiddleware = require('../middlewares/auth');
+
+// Lấy danh sách comment cho bài viết
+router.get('/:newsId', commentController.getCommentsByNewsId);
+
+// Thêm comment mới
+router.post('/', authMiddleware.requireAuth, commentController.createComment);
+
+//Thích/Bỏ thích comment
+router.post('/:commentId/like', authMiddleware.requireAuth, commentController.toggleLike);
+
+//Thêm route sửa, xóa comment
+router.put('/:commentId', authMiddleware.requireAuth, commentController.updateComment);
+router.delete('/:commentId', authMiddleware.requireAuth, commentController.deleteComment);
+
+
+module.exports = router;
